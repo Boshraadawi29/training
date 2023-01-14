5f739b11b047ef679a5df9baa91585ae895ec2c3
boshraadawi29 <boshraadawi4@gmail.com>
2023-01-14 15:37:33 +0300

    after deleting comments

--- binary.java
+++ binary.java
@@ -6,18 +6,7 @@ public class binary {
         int a = ob.nextInt();
         int[] bin = new int[32];
         int p = 0; //pointer
-        /*int [] value= {64,32,16,8,4,2,1};
-        int [] bin = {0,0,0,0,0,0,0};
 
-        for(int i = 0 ; i < value.length; i++ ){
-            if(value[i] <= a && a != 0){
-                bin[i] = 1;
-                a = a - value[i];
-            }
-        }
-        for (int i = 0 ; i < bin.length; i++){
-            System.out.print(bin[i] + "");
-        }*/
         while (a > 0) {
             bin[p++] = a % 2;
             a = a / 2;
